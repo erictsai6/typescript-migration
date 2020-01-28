e2cf7b9fd980449fb11f464057cf93e308f95085
Eric Tsai <eric.tsai@cbinsights.com>
2020-01-27 20:22:29 -0500

    Adjusted the code to actually use the arrays

--- main.sh
+++ main.sh
@@ -20,7 +20,8 @@ final_return_code=0
 
 validate_no_js() {
     local updated_type=$1
-    local files_changed=$2
+    shift
+    local files_changed=("$@")
     local js_files_array=()
     return_code=0
 
@@ -38,17 +39,16 @@ validate_no_js() {
                         fi
                     done
                     if [ $blacklisted -eq 0 ]; then
-                        js_files_array+=( $file_changed )
+                        js_files_array=("${js_files_array[@]}" "$file_changed")
                         return_code=1
                     fi
                 fi
             done
         fi       
     done
-
     if [[ $return_code -eq 1  ]]; then
         echo "  $updated_type:"        
-        for js_file in $js_files_array
+        for js_file in ${js_files_array[@]}
         do
             echo "      $js_file"
         done
@@ -57,29 +57,32 @@ validate_no_js() {
 
 echo "Started typescript migration"
 
-added_files=$(git diff $base_branch --name-status --diff-filter=A | awk '{print $2}' | tr "\n" "\n")
-validate_no_js "Added" $added_files
-final_return_code=$(($final_return_code+$return_code >= 1 ? 1 : 0))
-
-modified_files=$(git diff $base_branch --name-status --diff-filter=C | awk '{print $2}' | tr "\n" "\n")
-validate_no_js "Copied" $modified_files
+modified_files=$(git diff $base_branch --name-status --diff-filter=A | awk '{print $2}' | tr "\n" "\n")
+validate_no_js "Added" "${modified_files[@]}"
 final_return_code=$(($final_return_code+$return_code >= 1 ? 1 : 0))
 
 modified_files=$(git diff $base_branch --name-status --diff-filter=M | awk '{print $2}' | tr "\n" "\n")
-validate_no_js "Modified" $modified_files
+validate_no_js "Modified" "${modified_files[@]}"
 final_return_code=$(($final_return_code+$return_code >= 1 ? 1 : 0))
 
-modified_files=$(git diff $base_branch --name-status --diff-filter=R | awk '{print $2}' | tr "\n" "\n")
-validate_no_js "Renamed" $modified_files
+modified_files=$(git diff $base_branch --name-status --diff-filter=R | awk '{print $3}' | tr "\n" "\n")
+validate_no_js "Renamed" "${modified_files[@]}"
 final_return_code=$(($final_return_code+$return_code >= 1 ? 1 : 0))
 
-modified_files=$(git diff $base_branch --name-status --diff-filter=T | awk '{print $2}' | tr "\n" "\n")
-validate_no_js "Type Changed" $modified_files
-final_return_code=$(($final_return_code+$return_code >= 1 ? 1 : 0))
+# TODO(etsai) - I cannot seem to replicate these diff filter changes
+# modified_files=$(git diff $base_branch --name-status --diff-filter=C | awk '{print $2}' | tr "\n" "\n")
+# validate_no_js "Copied" "${modified_files[@]}"
+# final_return_code=$(($final_return_code+$return_code >= 1 ? 1 : 0))
+
+# modified_files=$(git diff $base_branch --name-status --diff-filter=T | awk '{print $2}' | tr "\n" "\n")
+# validate_no_js "Type Changed" "${modified_files[@]}"
+# final_return_code=$(($final_return_code+$return_code >= 1 ? 1 : 0))
 
 echo
 if [[ $final_return_code -eq 1 ]]; then 
     echo "error: JS(X)? files detected, be sure to update the files above before proceeding"
+else
+    echo "All touched files have been updated"
 fi
 echo "Exiting with code: $final_return_code"
 exit $final_return_code
